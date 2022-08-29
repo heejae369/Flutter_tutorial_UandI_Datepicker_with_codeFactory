e9b9cd5915cf149bf8ee34d761b63a2bedc4012a
heejae369 <51227242+heejae369@users.noreply.github.com>
2022-08-29 14:34:07 +0900

    with_stateManage
    theme manage in main.dart,
    _topPart change to stateless widget,

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:u_and_i_datepicker_practice/screen/home_screen.dart';
 
 void main() {
+
   runApp(
     MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'sunflower', //기본 폰트 지정가능,
+          // 따로 폰트를 지정하지 않는다면 해당 폰트로 적용됨 ,
+          // 그러면 기존의 TextFamily의 sunflower 폰트를 지울 수 있음
+        textTheme: TextTheme(
+          headline1: TextStyle( // text 'U&I'
+            color: Colors.white,
+            fontFamily: 'parisienne',
+            fontSize: 80.0,
+          ),
+
+          headline2: TextStyle( // text "D +"
+              color: Colors.white,
+              fontFamily: 'parisienne',
+              fontSize: 50.0,
+              fontWeight: FontWeight.w700),
+
+            bodyText1: TextStyle( // '우리처음만난날'
+              color: Colors.white,
+              fontSize: 30.0,
+            ),
+
+          bodyText2: TextStyle( // 년월일
+            color: Colors.white,
+            fontSize: 20.0,
+          ),
+
+        )
+      ),
         home: HomeScreen()
     ),
   );
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+//import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
   // 날짜가 변경되면 Dday도 변경되야 하기 때문에
@@ -10,6 +11,12 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  ); // 기본값은 현
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +26,10 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Container(
           width: MediaQuery.of(context).size.width, // 기기가 사용할 수 있는 너비를 가져옴
           child: Column(children: [
-            _TopPart(),
+            _TopPart(
+              selectedDate: selectedDate,
+              onPressed: onHeartPressed,
+            ),
             _BottomPart(),
           ] //children
               ),
@@ -27,27 +37,61 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+
+  onHeartPressed() {
+    final now = DateTime.now(); // 밑에서 너무 많이 쓰이니까 여기서 변수로 만듬
+
+    showCupertinoDialog(
+        context: context,
+        barrierDismissible: true, // 기본값은 False, 컨터이너 밖을 누르면 닫힘
+        builder: (BuildContext context) {
+          return Align(
+            alignment: Alignment.bottomCenter, // 아래쪽 가운데에 컨테이너 정렬
+            child: Container(
+              color: Colors.white,
+              height: 300.0, // 정렬을 설정하지 않으면 전체화면을 차지함
+              // 400만큼의 높이 차지
+              child: CupertinoDatePicker(
+                //
+                mode: CupertinoDatePickerMode.date,
+                initialDateTime: selectedDate,
+                maximumDate: DateTime(
+                    // 이것만 설정하면 에러남 이유, 쿠퍼티노 데이트 피커의 처음 선택된 시간은 실행되는 순간의 현시간의 DateTime.now()다
+                    // 이미 첫시작의 선택된 날짜가 maximum보다 크기에 에러가 남 그렇기에 위의 이니셜 데이트 타임
+                    now.year,
+                    now.month,
+                    now.day),
+                onDateTimeChanged: (DateTime date) {
+                  print(date); // TopPart부분에 변경해야할 위젯이 있기에 topPart stateful로 수정
+                  setState(() {
+                    //topPart는 stateless widget으로 바뀌어서 여기서 setState할 수 없다
+                    // stateful 위젯에서 변수를 변경시킬 때 빌드를 다시 실행시킬때 이 함수 사용
+                    selectedDate = date;
+                  });
+                }, //날짜나 시간이 바뀌었을 때, 무슨 액션을 줄지
+                // 시간함수의 파라미터로 DateTime date가 들어갈 것을 알 수 있다 날짜로 바꾸기에
+              ),
+            ),
+          );
+        });
+  }
 }
 
-class _TopPart extends StatefulWidget {
-  //_ 의 의미 이 파일안에서만 쓸 수 있도롥
-  const _TopPart({Key? key}) : super(key: key);
+class _TopPart extends StatelessWidget {
+  // selectedDate 가 위로 가면서 문제가 생김
+  //아래와 같은 코드로 문제 해결 TopPart에서 selectedDate 변수는 필수이다 라고 이야기한 것과 같다,
+  // 그러면 위에서 _TopPart()안에 변수를 넣어야한다
+  final DateTime selectedDate;
+  final VoidCallback onPressed;
 
-  @override
-  State<_TopPart> createState() => _TopPartState();
-}
-
-class _TopPartState extends State<_TopPart> {
-  DateTime selectedDate = DateTime(
-                   DateTime.now().year,
-                   DateTime.now().month,
-                   DateTime.now().day,
-
-  ); // 기본값은 현
+  _TopPart({required this.selectedDate, required this.onPressed, Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
+    final theme = Theme.of(context); //이 위젯트리에서 가장 가까운 위젯 테마 인스턴스를 가져올 수 있다
+    //Main.dart에 있는 theme는 HomeScreen보다 더 상위의 위젯에 위치해있다
+    final textTheme = theme.textTheme;
     final now = DateTime.now(); // 밑에서 너무 많이 쓰이니까 여기서 변수로 만듬
     //               DateTime.now().year,
     //               DateTime.now().month,
@@ -61,69 +105,24 @@ class _TopPartState extends State<_TopPart> {
         children: [
           Text(
             'U&I',
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'parisienne',
-              fontSize: 80.0,
-            ),
+            style: textTheme.headline1,
           ),
           Column(
             children: [
               Text(
                 '우리처음 만난 날',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'sunflower',
-                  fontSize: 30.0,
-                ),
+                style: textTheme.bodyText1,
               ),
               Text(
-                '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}', // setState() 에서 변경시킨 selectedDate 값을 바탕으로 바꿈
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'sunflower',
-                  fontSize: 20.0,
-                ),
+                '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
+                // setState() 에서 변경시킨 selectedDate 값을 바탕으로 바꿈
+                style: textTheme.bodyText2,
               ),
             ],
           ),
           IconButton(
             iconSize: 80.0,
-            onPressed: () {
-              showCupertinoDialog(
-                  context: context,
-                  barrierDismissible: true, // 기본값은 False, 컨터이너 밖을 누르면 닫힘
-                  builder: (BuildContext context) {
-                    return Align(
-                      alignment: Alignment.bottomCenter, // 아래쪽 가운데에 컨테이너 정렬
-                      child: Container(
-                        color: Colors.white,
-                        height: 300.0, // 정렬을 설정하지 않으면 전체화면을 차지함
-                        // 400만큼의 높이 차지
-                        child: CupertinoDatePicker(
-                          //
-                          mode: CupertinoDatePickerMode.date,
-                          initialDateTime: selectedDate,
-                          maximumDate: DateTime( // 이것만 설정하면 에러남 이유, 쿠퍼티노 데이트 피커의 처음 선택된 시간은 실행되는 순간의 현시간의 DateTime.now()다
-                            // 이미 첫시작의 선택된 날짜가 maximum보다 크기에 에러가 남 그렇기에 위의 이니셜 데이트 타임
-                            now.year,
-                            now.month,
-                            now.day
-                          ),
-                          onDateTimeChanged: (DateTime date) {
-                            print(
-                                date); // TopPart부분에 변경해야할 위젯이 있기에 topPart stateful로 수정
-                            setState(() {
-                              // stateful 위젯에서 변수를 변경시킬 때 빌드를 다시 실행시킬때 이 함수 사용
-                              selectedDate = date;
-                            });
-                          }, //날짜나 시간이 바뀌었을 때, 무슨 액션을 줄지
-                          // 시간함수의 파라미터로 DateTime date가 들어갈 것을 알 수 있다 날짜로 바꾸기에
-                        ),
-                      ),
-                    );
-                  });
-            },
+            onPressed: onPressed,
             icon: Icon(
               Icons.favorite,
               color: Colors.red,
@@ -131,17 +130,13 @@ class _TopPartState extends State<_TopPart> {
           ),
           Text(
             'D+${DateTime(
-              now.year,
-              now.month,
-              now.day,
-            ).difference(selectedDate).inDays + 1
-            }', // 선택한 날짜와 오늘의 날짜 차이를 입력해야함 // inDays 날짜 단위로 만들어줌 안하면 마이크로 초단위로 출력됨
-            
-            style: TextStyle(
-                color: Colors.white,
-                fontFamily: 'parisienne',
-                fontSize: 50.0,
-                fontWeight: FontWeight.w700),
+                  now.year,
+                  now.month,
+                  now.day,
+                ).difference(selectedDate).inDays + 1}',
+            style: textTheme.headline2,
+            // 선택한 날짜와 오늘의 날짜 차이를 입력해야함 // inDays 날짜 단위로 만들어줌 안하면 마이크로 초단위로 출력됨
+
           ),
         ],
       ),
